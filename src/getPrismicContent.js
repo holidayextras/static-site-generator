1c012a5114760077fdb6c9fe888103f7726496f5
Richard Stevens <richard.stevens@unknown.home>
2016-03-01 20:14:27 +0000

    Updating metalsmith-prismic and how we get html from fragments

--- src/getPrismicContent.js
+++ src/getPrismicContent.js
@@ -8,6 +8,15 @@ const getPrismicContent = ( ) => {
         if ( typeof pageData.data[data].json.value === 'string' ) {
           files[file].pageData[ data ] = pageData.data[data].json.value;
         }
+        // Get the data from fetchLinks fragments to save the html
+        if ( pageData.data[data].json && pageData.data[data].json.fragments ) {
+          Object.keys( pageData.data[data].json.fragments ).forEach( child => {
+            if ( typeof pageData.data[data].json.fragments[child].value === 'string' ) {
+              const newName = child.replace( pageData.data[data].json.type + '.', '' );
+              files[file].pageData[ data ][newName] = pageData.data[data].json.fragments[child].value;
+            }
+          } );
+        }
         delete files[file].prismic.page.results[0].data[data].json;
       } );
       files[file].pagename = file;
