d0619acccc898024035c0a20624467fa8693ff90
Richard Stevens <rsrichardstevens@googlemail.com>
2016-10-07 21:16:35 +0100

    Updating to support extra calls in hxseo

--- src/getHXSEOContent.js
+++ src/getHXSEOContent.js
@@ -8,28 +8,65 @@ const getHXSEOContent = ( opts ) => {
 
     const getDataForPage = ( res, fileName, cb ) => {
       let data = '';
-      res.on( 'data', ( d ) => {
+      res.on( 'data', d => {
         data += d;
       });
+      const newFiles = [ ];
       res.on( 'end', ( ) => {
         data = JSON.parse( data );
         for ( let i = 0; i < data.length; i++ ) {
           let newFile = clone( files[ fileName ] );
           newFile.pageData = data[i];
           files[ data[i].pagename + '.html' ] = newFile;
+          newFiles.push({
+            data: newFile,
+            key: data[i].pagename + '.html'
+          });
         }
         delete files[ fileName ];
-        cb( true );
+        cb( newFiles );
       });
     };
 
-    const callAPI = ( options, fileName, callBack ) => {
-      new Promise( ( resolve ) => {
-        http.get( options, ( res ) => {
+    const getExtraDataForPage = ( res, fileName, key, cb ) => {
+      let data = '';
+      res.on( 'data', d => {
+        data += d;
+      });
+      res.on( 'end', ( ) => {
+        data = JSON.parse( data );
+        files[ fileName ][ key ] = data.product;
+        cb( );
+      });
+    };
+
+    const asyncGetQuery = ( request, option, currentFile, opt, extraCallBack ) => {
+      if ( ! currentFile.data.pageData[option[1]] ) return extraCallBack( );
+      const value = currentFile.data.pageData[option[1]].split( ',' )[0];
+      request.path = request.path.replace( option[0], value );
+      http.get( request, ( resExtra ) => {
+        getExtraDataForPage( resExtra, currentFile.key, opt, extraCallBack );
+      });
+    };
+
+    const callAPI = ( options, fileName, fileParams, callBack ) => {
+      new Promise( resolve => {
+        http.get( options, res => {
           getDataForPage( res, fileName, resolve );
         });
-      }).then( ( ) => {
-        callBack( );
+      }).then( data => {
+        if ( ! fileParams.hxseo.extras ) return callBack( );
+        async.each( Object.keys( fileParams.hxseo.extras ), ( opt, extraCallBack ) => {
+          if ( ! fileParams.hxseo.extras[ opt ].path ) return extraCallBack( );
+          const option = fileParams.hxseo.extras[ opt ].path.match( /<%([^%].*)%>/ );
+          async.each( data, ( currentFile, cb ) => {
+            asyncGetQuery( fileParams.hxseo.extras[ opt ], option, currentFile, opt, cb );
+          }, ( ) => {
+            extraCallBack( );
+          });
+        }, ( ) => {
+          callBack( );
+        });
       });
     };
 
@@ -38,10 +75,8 @@ const getHXSEOContent = ( opts ) => {
       if ( !fileParams.hxseo ) return callBack( );
       const options = opts.url;
       options.path = fileParams.hxseo.query;
-      return callAPI( options, fileName, callBack );
-    }, error => {
-      done( error );
-    });
+      return callAPI( options, fileName, fileParams, callBack );
+    }, done );
   };
 };
 
