4949752b7cb064b6cd218896fdcb99e809a01662
Richard Stevens <richard.stevens@holidayextras.com>
2017-05-15 14:41:21 +0100

    Updating to move the initSetup function more globally accessible

--- src/getHXSEOContent.js
+++ src/getHXSEOContent.js
@@ -1,30 +1,30 @@
-import _ from 'underscore';
-import PageData from './components/pageData';
+import _ from 'underscore'
+import PageData from './components/pageData'
 
-const getHXSEOContent = ( opts ) => {
-  return ( files, metalsmith, done ) => {
-    if ( !opts.token ) throw ( 'Must provide a token for SEO api access' );
+const getHXSEOContent = (opts) => {
+  return (files, metalsmith, done) => {
+    if (!opts.token) throw (new Error('Must provide a token for SEO api access'))
     opts.token = {
       name: 'token',
       value: opts.token
-    };
+    }
+    opts.initSetup = params => {
+      // Extend params needed
+      if (!params.hxseo) return { }
+      params.dataSource = _.extend({ }, opts, params.hxseo)
+      params.dataSource.repeater = 'data'
+      params.dataSource.pageDataField = 'attributes'
+      params.dataSource.pageNameField = 'pageName'
+      return params
+    }
     new PageData({
       opts,
-      files,
-      initSetup: params => {
-        // Extend params needed
-        if ( !params.hxseo ) return { };
-        params.dataSource = _.extend( { }, opts, params.hxseo );
-        params.dataSource.repeater = 'data';
-        params.dataSource.pageDataField = 'attributes';
-        params.dataSource.pageNameField = 'pageName';
-        return params;
-      }
-    }).then( ( data ) => {
-      files = data;
-      done( );
-    });
-  };
-};
+      files
+    }).then((data) => {
+      files = data
+      done()
+    })
+  }
+}
 
-export default getHXSEOContent;
+export default getHXSEOContent
