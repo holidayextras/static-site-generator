e22d9c450f76104cc1ee49f6f6f12252a46d9d4c
Richard Stevens <richard.stevens@holidayextras.com>
2018-12-12 15:07:33 +0000

    Updating to support utf16 chars

--- src/components/pageData.js
+++ src/components/pageData.js
@@ -65,6 +65,11 @@ const PageData = class PageData {
       })
       res.on('end', () => {
         try {
+          data = data.split('').map(char => char.charCodeAt(0).toString().length < 4 ? char : '').join('').replace(/(\\u|\\x|\\d)\d{4}/gm, '')
+        } catch (e) {
+          console.log(e, 'Can\'t convert charCodeAt')
+        }
+        try {
           data = JSON.parse(data)
           if (!data) return reject(new Error('Nothing returned'))
           if (data.message) return reject(data.message)
