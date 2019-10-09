66b6c7a8e5c41a08178d03808e71b028c4f4dd0a
Warren Fitzpatrick <warren.fitzpatrick@holidayextras.com>
2019-10-09 12:02:29 +0100

    Adjusted pageNameChanger to allow for page prefix and suffix

--- src/components/pageNameChanger.js
+++ src/components/pageNameChanger.js
@@ -1,18 +1,18 @@
 const pageNameChanger = (pageName, fileParams) => {
-  if (fileParams.postPageNameChange) {
-    if (fileParams.postPageNameChange === 'strip') {
-      pageName = pageName.split('/')
-      pageName = pageName[pageName.length - 1].replace(/[\d]+/, '')
-      let pageNames = pageName.split('-')
-      const start = pageNames[0] === '' ? 1 : 0
-      pageNames = pageNames.slice(start, pageNames.length)
-      if (pageNames[pageNames.length - 1] === '') {
-        pageNames = pageNames.slice(0, pageNames.length - 1)
-      }
-      pageName = pageNames.join('-').toLowerCase().replace(/--+/, '-')
+  const { postPageNameChange = '', fileNamePrefix = '', fileNameSuffix = '' } = fileParams
+  if (postPageNameChange !== 'strip') return `${fileNamePrefix}${pageName}${fileNameSuffix}`
+  else {
+    pageName = pageName.split('/')
+    pageName = pageName[pageName.length - 1].replace(/[\d]+/, '')
+    let pageNames = pageName.split('-')
+    const start = pageNames[0] === '' ? 1 : 0
+    pageNames = pageNames.slice(start, pageNames.length)
+    if (pageNames[pageNames.length - 1] === '') {
+      pageNames = pageNames.slice(0, pageNames.length - 1)
     }
+    pageName = pageNames.join('-').toLowerCase().replace(/--+/, '-')
+    return `${fileNamePrefix}${pageName}${fileNameSuffix}`
   }
-  return pageName
 }
 
 export default pageNameChanger
