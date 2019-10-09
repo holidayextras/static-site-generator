a2d020f59152ead5558a6179ed2aa1be4d79d23f
Warren Fitzpatrick <warren.fitzpatrick@holidayextras.com>
2019-10-09 13:16:35 +0100

    Updated pageNameChanger

--- src/components/pageNameChanger.js
+++ src/components/pageNameChanger.js
@@ -1,16 +1,17 @@
 const pageNameChanger = (pageName, fileParams) => {
   const { postPageNameChange = '', fileNamePrefix = '', fileNameSuffix = '' } = fileParams
-  if (postPageNameChange !== 'strip') return `${fileNamePrefix}${pageName}${fileNameSuffix}`
-
-  pageName = pageName.split('/')
-  pageName = pageName[pageName.length - 1].replace(/[\d]+/, '')
-  let pageNames = pageName.split('-')
-  const start = pageNames[0] === '' ? 1 : 0
-  pageNames = pageNames.slice(start, pageNames.length)
-  if (pageNames[pageNames.length - 1] === '') {
-    pageNames = pageNames.slice(0, pageNames.length - 1)
+  if (!postPageNameChange) return `${fileNamePrefix}${pageName}${fileNameSuffix}`
+  if (postPageNameChange === 'strip') {
+    pageName = pageName.split('/')
+    pageName = pageName[pageName.length - 1].replace(/[\d]+/, '')
+    let pageNames = pageName.split('-')
+    const start = pageNames[0] === '' ? 1 : 0
+    pageNames = pageNames.slice(start, pageNames.length)
+    if (pageNames[pageNames.length - 1] === '') {
+      pageNames = pageNames.slice(0, pageNames.length - 1)
+    }
+    pageName = pageNames.join('-').toLowerCase().replace(/--+/, '-')
   }
-  pageName = pageNames.join('-').toLowerCase().replace(/--+/, '-')
   return `${fileNamePrefix}${pageName}${fileNameSuffix}`
 }
 
