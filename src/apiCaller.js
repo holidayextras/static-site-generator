c642c90edc1e298ffc157a097bac9e846157bf43
Andrew Griffiths <andrew.griffiths@holidayextras.com>
2020-03-19 15:37:43 +0000

    postQuery support for api

--- src/apiCaller.js
+++ src/apiCaller.js
@@ -1,13 +1,26 @@
 import PageData from './components/pageData'
+import path from 'path'
 
 const apiCaller = (opts) => {
   return (files, metalsmith, done) => {
+    const postQuery = opts.url && opts.url.postBuild || false
     new PageData({
       opts,
       files
     }).then((data) => {
-      files = data
-      done()
+      const newDone = data => {
+        files = data
+        done()
+      }
+      if (postQuery) {
+        const pathToFunc = path.join(metalsmith._directory, postQuery)
+        const func = require(pathToFunc)
+        if (typeof func !== 'function') return newDone(data)
+        func(data).then(newDone).catch(function (err) {
+          console.log(err)
+          newDone(data)
+        })
+      } else newDone(data)
     })
   }
 }
