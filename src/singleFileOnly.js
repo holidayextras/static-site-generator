2610ba854aa8155e51fd81661e7115bc7594e3a7
Tom Price <tom.price@holidayextras.com>
2021-12-01 07:31:01 +0000

    Fix singlePage for non prefixed

--- src/singleFileOnly.js
+++ src/singleFileOnly.js
@@ -3,8 +3,8 @@ const singleFileOnly = (opts) => {
     if (!process.env.srcFile && !process.env.singlePage) return done()
     if (process.env.singlePage) {
       Object.keys(files).map(fileName => {
-        const pagePrefix = opts.webpackOptions.folderPrefix ? opts.webpackOptions.folderPrefix.replace(/^\//, '') : ''
-        const singlePage = pagePrefix + '/' + process.env.singlePage + '.html'
+        const pagePrefix = opts.webpackOptions.folderPrefix ? opts.webpackOptions.folderPrefix.replace(/^\//, '') + '/' : ''
+        const singlePage = pagePrefix + process.env.singlePage + '.html'
         if (fileName !== singlePage) delete files[fileName]
         return fileName
       })
