970e9049a47006eed0e50bbeb5d79d05626d7432
leonel sa <leonel.sa@bytepitch.com>
2025-03-10 17:31:43 +0000

    Fix single pages with names that start with the folderPrefix

--- src/singleFileOnly.js
+++ src/singleFileOnly.js
@@ -3,7 +3,8 @@ const singleFileOnly = (opts) => {
     if (!process.env.srcFile && !process.env.singlePage) return done()
     if (process.env.singlePage) {
       Object.keys(files).map(fileName => {
-        const pagePrefix = opts.webpackOptions.folderPrefix ? opts.webpackOptions.folderPrefix.replace(/^\//, '') + '/' : ''
+        const folderPrefix = opts.webpackOptions.folderPrefix ? `${opts.webpackOptions.folderPrefix.replace(/^\//, '')}/` : null
+        const pagePrefix = folderPrefix && !process.env.singlePage.startsWith(folderPrefix) ? folderPrefix : ''
         const singlePage = `${pagePrefix}${process.env.singlePage}.html`
         if (fileName !== singlePage) delete files[fileName]
         return fileName
